256bd5ea3fedaf6f3686325602370d3d789566c5
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2023-05-09 14:51:02 +0200

    ✨ Legger til flere dato funksjoner

--- packages/aap-felles-utils-client/src/date/date.test.ts
+++ packages/aap-felles-utils-client/src/date/date.test.ts
@@ -1,4 +1,10 @@
-import { formaterDatoForVisning, formaterDatoTidForVisning } from './date';
+import {
+  formaterDatoForVisning,
+  formaterDatoMedDag,
+  formaterDatoTidForVisning,
+  formaterDatoUtenDag,
+  formaterDatoUtenÅr,
+} from './date';
 
 describe('formaterDatoForVisning', () => {
   it('skal returnere formatert dato når man sender inn gyldig ISO string med tidsstempel', () => {
@@ -26,5 +32,53 @@ describe('formaterDatoTidForVisning', () => {
   it('skal returner formatert dato og tidsstempel når man sender inn et datoobjekt', () => {
     const dato = new Date('2022-03-25T12:22:43');
     expect(formaterDatoTidForVisning(dato)).toBe('25.03.2022 12:22');
+  });
+});
+
+describe('formaterDatoMedDag', () => {
+  it('skal returnere formatert dato når man sender inn en string', function () {
+    const dato = '2022-03-25T12:22:43';
+    expect(formaterDatoMedDag(dato)).toBe('25. mars 2022');
+  });
+
+  it('skal returnere formatert dato når man sender inn et datoobjekt', function () {
+    const dato = '2022-03-25T12:22:43';
+    expect(formaterDatoMedDag(new Date(dato))).toBe('25. mars 2022');
+  });
+});
+
+describe('formaterDatoMedDag', () => {
+  it('skal returnere formatert dato når man sender inn en string', function () {
+    const dato = '2022-03-25T12:22:43';
+    expect(formaterDatoMedDag(dato)).toBe('25. mars 2022');
+  });
+
+  it('skal returnere formatert dato når man sender inn et datoobjekt', function () {
+    const dato = '2022-03-25T12:22:43';
+    expect(formaterDatoMedDag(new Date(dato))).toBe('25. mars 2022');
+  });
+});
+
+describe('formaterDatoUtenDag', () => {
+  it('skal returnere formatert dato når man sender inn en string', function () {
+    const dato = '2022-03-25T12:22:43';
+    expect(formaterDatoUtenDag(dato)).toBe('mars 2022');
+  });
+
+  it('skal returnere formatert dato når man sender inn et datoobjekt', function () {
+    const dato = '2022-03-25T12:22:43';
+    expect(formaterDatoUtenDag(new Date(dato))).toBe('mars 2022');
+  });
+});
+
+describe('formaterDatoUtenÅr', () => {
+  it('skal returnere formatert dato når man sender inn en string', function () {
+    const dato = '2022-03-25T12:22:43';
+    expect(formaterDatoUtenÅr(dato)).toBe('25. mars');
+  });
+
+  it('skal returnere formatert dato når man sender inn et datoobjekt', function () {
+    const dato = '2022-03-25T12:22:43';
+    expect(formaterDatoUtenÅr(new Date(dato))).toBe('25. mars');
   });
 });
--- packages/aap-felles-utils-client/src/date/date.ts
+++ packages/aap-felles-utils-client/src/date/date.ts
@@ -1,6 +1,11 @@
 import { format, parseISO } from 'date-fns';
 import { nb } from 'date-fns/locale';
 
+/**
+ * This is my cool function
+ *
+ * @param dato
+ */
 export function formaterDatoForVisning(dato: string | Date): string {
   if (typeof dato === 'string') {
     return format(parseISO(dato), 'dd.MM.yyyy', { locale: nb });
@@ -14,5 +19,29 @@ export function formaterDatoTidForVisning(dato: string | Date): string {
     return format(new Date(dato), 'dd.MM.yyyy HH:mm');
   } else {
     return format(dato, 'dd.MM.yyyy HH:mm');
+  }
+}
+
+export function formaterDatoMedDag(dato: string | Date): string {
+  if (typeof dato === 'string') {
+    return format(new Date(dato), 'dd. LLLL yyyy', { locale: nb });
+  } else {
+    return format(dato, 'dd. LLLL yyyy', { locale: nb });
+  }
+}
+
+export function formaterDatoUtenDag(dato: string | Date): string {
+  if (typeof dato === 'string') {
+    return format(new Date(dato), 'LLLL yyyy', { locale: nb });
+  } else {
+    return format(dato, 'LLLL yyyy', { locale: nb });
+  }
+}
+
+export function formaterDatoUtenÅr(dato: string | Date): string {
+  if (typeof dato === 'string') {
+    return format(new Date(dato), 'dd. LLLL', { locale: nb });
+  } else {
+    return format(dato, 'dd. LLLL', { locale: nb });
   }
 }
