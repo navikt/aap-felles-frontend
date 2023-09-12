96b5becee22ed47e50e4da502eaaf34a27835c44
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2023-09-12 13:25:20 +0200

    :lipstick: Oppgraerer Storybook pakker
    Co-authored-by: Tor Idland <tor.idland@nav.no>
    Co-authored-by: Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,33 @@
+import { dirname, join } from "path";
 module.exports = {
   stories: [
     "../packages/aap-felles-react/**/*.stories.mdx",
     "../packages/aap-felles-react/**/*.stories.tsx",
   ],
+
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
   ],
-  framework: "@storybook/react",
-  core: {
-    disableTelemetry: true, // 👈 Disables telemetry
-    builder: "webpack5",
+
+  framework: {
+    name: getAbsolutePath("@storybook/nextjs"),
+    options: {
+
+    }
   },
+
+  core: {
+    // 👈 Disables telemetry
+    disableTelemetry: true
+  },
+
+  docs: {
+    autodocs: true
+  }
 };
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
