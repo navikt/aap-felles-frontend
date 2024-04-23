37413a0cad6dfb9d54d3f18edd8b0c7851919c8f
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2024-04-23 10:01:34 +0200

    Legger til changeset for versjonering

--- packages/aap-felles-utils/src/environments/environments.ts
+++ packages/aap-felles-utils/src/environments/environments.ts
@@ -1,3 +1,5 @@
 const MOCK_ENVIRONMENTS = ['localhost', 'labs'];
 
 export const isMock = () => MOCK_ENVIRONMENTS.includes(process.env.NEXT_PUBLIC_ENVIRONMENT ?? '');
+
+export const test = () => console.log('test');
