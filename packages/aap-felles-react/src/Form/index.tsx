9b8ae09c55676a609f5c029293ef2e3a43c0db71
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-09-18 09:05:23 +0200

    Eksporterer alle input wrappers

--- packages/aap-felles-react/src/Form/index.tsx
+++ packages/aap-felles-react/src/Form/index.tsx
@@ -1,2 +1,10 @@
 export * from './FormField';
 export * from './FormHook';
+export * from './textfieldwrapper/TextFieldWrapper'
+export * from './checkboxwrapper/CheckboxWrapper'
+export * from './datewrapper/DateWrapper'
+export * from './radiogroupwrapper/RadioGroupWrapper'
+export * from './textareawrapper/TextAreaWrapper'
+export * from './comboboxwrapper/ComboboxWrapper'
+export * from './selectwrapper/SelectWrapper'
+
