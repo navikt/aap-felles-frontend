f626081ad3ecb8aa75f84dcbe7c09d0c64677106
Fredrik Meyer <fredrik.meyer@nav.no>
2024-10-11 11:44:15 +0200

    Update nb.ts

--- packages/aap-felles-react/src/FileInput/nb.ts
+++ packages/aap-felles-react/src/FileInput/nb.ts
@@ -7,7 +7,7 @@ export default {
     fileTooLarge: 'Filen(e) du lastet opp er for stor. Last opp fil(er) med maksimal samlet størrelse 50 MB.',
     virus: 'Det er oppdaget virus på filen du prøver å laste opp. Velg en annen fil å laste opp.',
     size: 'Maksimal samlet størrelse på vedlegg per bruker(50MB) er oversteget.',
-    default422: 'Noe gikk galt i prosesseringen av filen. Prøv å laste opp dokumentet som pdf i stedet.',
+    default422: 'Noe gikk galt i prosesseringen av filen. Prøv å laste opp dokumentet som PDF i stedet.',
     passordbeskyttet:
       'Filen er passord-beskyttet og vil ikke kunne leses av en saksbehandler, fjern beskyttelsen og prøv igjen',
     other: 'Opplastingen feilet. Prøv på nytt',
