aafcc405cdd7e076c5f5c2481e16f3631ca11e4b
Nicolas N <nicolas.nordhagen@nav.no>
2025-03-06 11:13:20 +0100

    Fikse brukket design på header, ta i bruk mer fra aksel, + småplukk

--- packages/aap-felles-react/src/KelvinAppHeader/Kelvinsøkeresultat.tsx
+++ packages/aap-felles-react/src/KelvinAppHeader/Kelvinsøkeresultat.tsx
@@ -1,41 +1,39 @@
 'use client'
 
-import {HStack, Label} from "@navikt/ds-react";
+import { Detail, HStack, Label, Link, VStack } from "@navikt/ds-react";
 import React from "react";
-import {SøkeResultat} from "./Kelvinsøk";
+import { SøkeResultat } from "./Kelvinsøk";
 
 interface Props {
   søkeresultat: SøkeResultat
 }
-export const Kelvinsøkeresultat = ({søkeresultat}: Props) => {
-  return (
-    <section>
-      <HStack gap={'4'}>
-        {(søkeresultat?.saker && søkeresultat?.saker.length > 0) && (<div>
-          <Label>Saker</Label>
-          <ul className={'kelvin-oppgavesok-resultat'}>
-            {søkeresultat.saker.map((søk, index) => (
-              <li key={`saker-resultat-${index}`}>
-                <a
-                  href={søk.href}
-                >{søk.label}</a>
-              </li>
-            ))}
-          </ul>
-        </div>)}
-        {(søkeresultat?.oppgaver && søkeresultat?.oppgaver.length > 0) && (<div>
-          <Label>Oppgaver</Label>
-          <ul className={'kelvin-oppgavesok-resultat'}>
-            {søkeresultat.oppgaver.map((søk, index) => (
-              <li key={`oppgaver-resultat-${index}`}>
-                <a
-                  href={søk.href}
-                >{søk.label}</a>
-              </li>
-            ))}
-          </ul>
-        </div>)}
-      </HStack>
-    </section>
-  )
-}
+
+export const Kelvinsøkeresultat = ({ søkeresultat: { oppgaver, saker } }: Props) => (
+  <HStack gap={'8'}>
+    <div>
+      <Label spacing>Saker</Label>
+      <VStack gap="2">
+        {!saker?.length ? (
+          <Detail>Fant ingen saker</Detail>
+        ) : (
+          saker.map((søk, index) => (
+            <Link key={`sak-resultat-${index}`} href={søk.href}>{søk.label}</Link>
+          ))
+        )}
+      </VStack>
+    </div>
+
+    <div>
+      <Label spacing>Oppgaver</Label>
+      <VStack gap="2">
+        {!oppgaver?.length ? (
+          <Detail>Fant ingen saker</Detail>
+        ) : (
+          oppgaver.map((søk, index) => (
+            <Link key={`oppgave-resultat-${index}`} href={søk.href}>{søk.label}</Link>
+          ))
+        )}
+      </VStack>
+    </div>
+  </HStack>
+)
