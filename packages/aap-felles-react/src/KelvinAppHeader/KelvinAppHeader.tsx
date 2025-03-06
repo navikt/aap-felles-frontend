afd9abaa4030433c98106fa52bc1f40530125857
Nicolas N. <Watercolours@users.noreply.github.com>
2025-03-06 11:15:22 +0100

    Fikse brukket design på header, ta i bruk mer fra aksel, + småplukk (#60)

--- packages/aap-felles-react/src/KelvinAppHeader/KelvinAppHeader.tsx
+++ packages/aap-felles-react/src/KelvinAppHeader/KelvinAppHeader.tsx
@@ -1,9 +1,11 @@
 'use client';
 
-import React, {useState} from 'react';
-import {Button, Dropdown, HStack, InternalHeader, Link, VStack} from '@navikt/ds-react';
-import { Kelvinsøk, SøkeResultat} from './Kelvinsøk';
-import {Kelvinsøkeresultat} from "./Kelvinsøkeresultat";
+import React, { useState } from 'react';
+import { Box, Button, Dropdown, Heading, HStack, InternalHeader, Link, Spacer, VStack } from '@navikt/ds-react';
+import { Kelvinsøk, SøkeResultat } from './Kelvinsøk';
+import { LeaveIcon, XMarkIcon } from '@navikt/aksel-icons';
+import { Kelvinsøkeresultat } from "./Kelvinsøkeresultat";
+
 interface BrukerInformasjon {
   navn: string;
   NAVident?: string;
@@ -15,47 +17,66 @@ const Brukermeny = ({ brukerInformasjon }: { brukerInformasjon: BrukerInformasjo
     <Dropdown.Menu>
       <Dropdown.Menu.List>
         <Dropdown.Menu.List.Item>
-          <Link href={'/oauth2/logout'} className="kelvin-app-header-link">
-            Logg ut
-          </Link>
+          <Link href={'/oauth2/logout'}>Logg ut</Link>
+          <Spacer />
+          <LeaveIcon aria-hidden fontSize="1.5rem" />
         </Dropdown.Menu.List.Item>
       </Dropdown.Menu.List>
     </Dropdown.Menu>
   </Dropdown>
 );
+
 export const KelvinAppHeader = ({ brukerInformasjon }: { brukerInformasjon: BrukerInformasjon }) => {
   const [søkeresultat, setSøkeresultat] = useState<SøkeResultat | undefined>(undefined);
+
   return (
-    <InternalHeader className="kelvin-app-header">
-      <div className={`kelvin-app-main-header ${søkeresultat ? 'kelvin-app-main-header-borderBottom' : ''}`}>
-        <div className="kelvin-app-header-leftSide">
-          <InternalHeader.Title href="/">Kelvin</InternalHeader.Title>
-          <Kelvinsøk setSøkeresultat={setSøkeresultat}/>
-          <Link className="kelvin-app-header-link" href={`/oppgave/`}>
+    <>
+      <InternalHeader>
+        <InternalHeader.Title href="/">Kelvin</InternalHeader.Title>
+
+        <HStack gap="4" marginInline="4">
+          <Kelvinsøk setSøkeresultat={setSøkeresultat} />
+          <Link href={`/oppgave/`} className="kelvin-app-header-link">
             Oppgaveliste
           </Link>
-          <Link className="kelvin-app-header-link" href={`/oppgave/produksjonsstyring`}>
+          <Link href={`/oppgave/produksjonsstyring`} className="kelvin-app-header-link">
             Produksjonsstyring
           </Link>
-          <Link className="kelvin-app-header-link" href={`/saksbehandling/saksoversikt`}>
+          <Link href={`/saksbehandling/saksoversikt`} className="kelvin-app-header-link">
             Saksoversikt
           </Link>
-          <Link className="kelvin-app-header-link" href={`/postmottak/`}>
+          <Link href={`/postmottak/`} className="kelvin-app-header-link">
             Postmottak
           </Link>
-          <Link className="kelvin-app-header-link" href={`/saksbehandling/sanity`}>
+          <Link href={`/saksbehandling/sanity`} className="kelvin-app-header-link">
             Sanity
           </Link>
-        </div>
-        <Brukermeny brukerInformasjon={brukerInformasjon}/>
-      </div>
-      {søkeresultat && <VStack padding={'2'}>
-          <HStack justify={'space-between'} >
-              <p>Søkeresultater</p>
-              <Button variant={'secondary'} size={'small'} onClick={() => setSøkeresultat(undefined)}>Lukk</Button>
-          </HStack>
-          <Kelvinsøkeresultat søkeresultat={søkeresultat}/>
-      </VStack>}
-    </InternalHeader>
+        </HStack>
+
+        <Spacer />
+        <Brukermeny brukerInformasjon={brukerInformasjon} />
+      </InternalHeader>
+
+      {søkeresultat && (
+        <Box background="surface-inverted" className="kelvin-app-header-search-result">
+          <VStack padding="4">
+            <HStack justify={'space-between'}>
+              <Heading size="small" spacing>Søkeresultater</Heading>
+              <Button
+                variant={'primary-neutral'}
+                size={'small'}
+                icon={<XMarkIcon />}
+                onClick={() => setSøkeresultat(undefined)}
+              >
+                Lukk
+              </Button>
+            </HStack>
+
+            <Kelvinsøkeresultat søkeresultat={søkeresultat} />
+          </VStack>
+        </Box>
+      )}
+
+    </>
   );
 }
