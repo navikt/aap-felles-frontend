6454f8c7bfc8f07f3222dda20c7499d066832c1c
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2025-03-12 16:18:09 +0100

    Fjerner form.css fra felles lib

--- packages/aap-felles-react/src/index.ts
+++ packages/aap-felles-react/src/index.ts
@@ -2,4 +2,3 @@ export * from './LucaGuidePanel/LucaGuidePanel';
 export * from './ScanningGuide/ScanningGuide';
 export * from './FileInput/FileInput';
 export * from './FileInput/FileInputInnsending';
-export * from './Form';
